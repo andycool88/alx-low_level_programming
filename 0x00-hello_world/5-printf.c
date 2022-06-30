ff9cae9ad60252d5b9ee9bc8a53817e552af0bab
Emaye Andrew Olugbenga <g.emaye@live.com>
2022-06-30 03:08:14 -0700

    5print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
-#!/bin/bash
 #include <stdio.h>
+#include <stdlib.h>
 /**
  *
  *  * main - Entry point
@@ -10,9 +10,7 @@
  *
  *     */
 
-int main(void)
-
-{
+int main(void) {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	    return (0);
 }
