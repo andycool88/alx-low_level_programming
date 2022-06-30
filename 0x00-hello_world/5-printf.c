9cc549d4a6e60052b9b0262bc04b33f79ab2c0ce
Emaye Andrew Olugbenga <g.emaye@live.com>
2022-06-30 03:22:04 -0700

    5print c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
  *
- *  * main - Entry point
+ *  * main - main block
  *
- *   *
+ *   * Return: 0
  *
- *    * Return: Always 0 (Success)
- *
- *     */
+ *    */
 
-int main(void) {
+int main(void)
+
+{
+
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	    return (0);
+
+	return (0);
+
 }
